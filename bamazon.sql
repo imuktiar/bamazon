ffb661c6d176301e1c5dde595f731a66368ccf22
Muktiar Idrees <44882857+imuktiar@users.noreply.github.com>
2019-04-01 19:52:45 -0400

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -11,15 +11,12 @@ create table products(
 );
 
 insert into products(product_name, department_name, price, stock_quantity)
-values("GTX 1080ti", "Electronics", 650, 100), 
-("Coilovers", "Car Parts", 700, 30), 
-("Jordans", "Shoes", 160, 50), 
-("Entry Table", "Furniture", 100, 80), 
-("Lifx", "Lighting", 35, 150),
-("Phillips Hue", "Lighting", 30, 100), 
-("Bed", "Furniture", 500, 20), 
-("Coffee Maker", "Kitchen", 150, 150), 
-("Poster", "Decoration", 7, 300),
-("Cat Toy", "Pet Section", 3, 300);
+values 
+("Diffuser", "Car Parts", 700, 30), 
+("Adidas", "Shoes", 160, 50), 
+("Persol", "Frames", 100, 80), 
+("Sharpie", "Office", 35, 150),
+("Screw driver", "Home", 30, 100), 
+("Sofa", "Furniture", 500, 20), 
 
 select * from products; 
